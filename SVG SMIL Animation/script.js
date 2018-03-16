a637178deb2603169f2fd189aa8125e79c2a3d52
Gabriele Corti <33316703+borntofrappe@users.noreply.github.com>
2018-03-16 01:24:58 +0100

    Update script.js
    Include comments

--- SVG SMIL Animation/script.js
+++ SVG SMIL Animation/script.js
@@ -1,14 +1,23 @@
+/*
+target svg elements on which to listen for the click event
+*/
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
 
+/*
+target animate tags in order to trigger their animation
+*/
 const leftArrowCircleAnimate = document.querySelectorAll(".left-arrow__circle--animate");
 const rightArrowCircleAnimate = document.querySelectorAll(".right-arrow__circle--animate");
-
 
 const leftArrowRectangleAnimate = document.querySelector(".placeholder__rectangle--animate--left");
 const rightArrowRectangleAnimate = document.querySelector(".placeholder__rectangle--animate--right");
 
+/*
+listen for a click event on each element, at which point trigger the animation, on the prescribed animation tags
+*/
 leftArrow.addEventListener("click", function() {
+  // loop through all selectors of the same class
   leftArrowCircleAnimate.forEach(function(animateAttribute) {
     animateAttribute.beginElement();
   });
@@ -16,6 +25,7 @@ leftArrow.addEventListener("click", function() {
 });
 
 rightArrow.addEventListener("click", function() {
+  // loop through all selectors of the same class
   rightArrowCircleAnimate.forEach(function(animateAttribute) {
     animateAttribute.beginElement();
   });
