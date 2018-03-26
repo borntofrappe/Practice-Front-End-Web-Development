7705ae2eab2d0941b617c812de84d8ef59eb58c4
Gabriele Corti <33316703+borntofrappe@users.noreply.github.com>
2018-03-26 14:36:37 +0200

    HTML, CSS, SVG assets
    Include files for the local project

---
+++ Background Gradients and Scrolling Animation/script.js
@@ -0,0 +1,14 @@
+// script written while testing out the project just to practice with the .forEach() method
+// the animation is inclued directly through CSS
+
+/* 
+
+const optionsTwo = document.querySelectorAll(".option--two");
+
+optionsTwo.forEach(function(optionTwo) {
+    optionTwo.classList.add("roll-in-animation");
+});
+
+*/
+
+
