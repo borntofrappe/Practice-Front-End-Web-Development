3c7cefea19b9ceef231cc0315ff7947bbb1007d7
Gabriele Corti <33316703+borntofrappe@users.noreply.github.com>
2018-05-26 22:49:10 +0200

    target the sup and sub elements of the paragraph

--- CodePenChallenge Sup Sub/script.js
+++ CodePenChallenge Sup Sub/script.js
@@ -1,6 +1,6 @@
-// target all sup and sub elements
-const sup = document.querySelectorAll("sup");
-const sub = document.querySelectorAll("sub");
+// target all sup and sub elements in the paragraph element
+const sup = document.querySelectorAll("p sup");
+const sub = document.querySelectorAll("p sub");
 
 // add an event listener on all sup and sub elements, at which point call a function which removes the choice not made and displays the correct text in a span
 sup.forEach(sup => sup.addEventListener("click", displayChoice));
