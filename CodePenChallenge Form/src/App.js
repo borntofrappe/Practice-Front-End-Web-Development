d7aacbc1ba386a469f037ac39ff58c3680a88d84
gabrielecorti <borntofrappe@gmail.com>
2018-07-14 00:00:29 +0200

    include initial progress

--- CodePenChallenge Form/src/App.js
+++ CodePenChallenge Form/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import SignUpForm from './SignUpForm';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <SignUpForm />
       </div>
     );
   }
---
+++ CodePenChallenge Form/src/SignUpForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './SignUpForm.css';
+import InputField from './InputField';
+import { CSSTransitionGroup } from 'react-transition-group';
+
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputName: 'fname'
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+
+    e.preventDefault();
+    let inputName = this.state.inputName;
+    switch(inputName) {
+      case 'fname':
+        inputName = 'lname';
+        break;
+      case 'lname':
+        inputName = 'email';
+        break;
+      default:
+        inputName = 'err';
+        break;
+    }
+    this.setState({
+      inputName: inputName
+    });
+  }
+
+
+  render() {
+    return(
+    
+    <form className="SignUpForm">
+      <h1>Sign up</h1>
+
+      {
+        this.state.inputName === "fname" 
+      && 
+        <InputField name="fname" label="First Name" type="text" />
+      }
+
+      {
+        this.state.inputName === "lname" 
+      && 
+        <InputField name="lname" label="Last Name" type="text" />
+      }
+
+      {
+        this.state.inputName === "email" 
+      && 
+        <InputField name="email" label="Email Address" type="email" />
+      }
+      {
+        this.state.inputName === "err"
+      &&
+        <div className="ErrorForm">
+          <p>Ehm...forgot to register</p>
+          <p>Gimme everything at once</p>
+          <InputField name="fname" label="First Name" type="text" />
+          <InputField name="lname" label="Last Name" type="text" />
+          <InputField name="email" label="Email Address" type="email" />
+        </div>
+      }
+
+     
+
+      <button type="submit" onClick = {this.handleClick}>Submit</button>
+    </form>
+    );
+  }
+} 
+
+export default SignUpForm;
