f7d2b8fd3420b4001bbb1f8637467a8c20df2f78
gabrielecorti <borntofrappe@gmail.com>
2018-07-16 16:54:02 +0200

    include starter files (without node modules)

---
+++ React To-Do App/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>ToDo App</h1>
+    );
+  }
+}
+
+export default App;
