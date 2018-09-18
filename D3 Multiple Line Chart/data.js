a881a5bf81ca92faeaedff9f159084083b6bede6
gabrielecorti <borntofrappe@gmail.com>
2018-09-18 23:21:41 +0200

    include working files

---
+++ D3 Multiple Line Chart/data.js
@@ -0,0 +1,436 @@
+const data = [
+  {
+      trimester: "T1.2003",
+      fifteenToTwentyFour: 17.8,
+      twentyFiveToFortyNine: 8,
+      aboveFortyNine: 4.9,
+      total: 8.4
+  },
+  {
+      trimester: "T2.2003",
+      fifteenToTwentyFour: 19,
+      twentyFiveToFortyNine: 8,
+      aboveFortyNine: 5.1,
+      total: 8.5,
+  },
+  {
+      trimester: "T3.2003",
+      fifteenToTwentyFour: 18.8,
+      twentyFiveToFortyNine: 7.8,
+      aboveFortyNine: 5.4,
+      total: 8.4
+  },
+  {
+      trimester: "T4.2003",
+      fifteenToTwentyFour: 20.1,
+      twentyFiveToFortyNine: 8.1,
+      aboveFortyNine: 5.5,
+      total: 8.8
+  },
+  {
+      trimester: "T1.2004",
+      fifteenToTwentyFour: 20.3,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 5.7,
+      total: 9
+  },
+  {
+      trimester: "T2.2004",
+      fifteenToTwentyFour: 20.3,
+      twentyFiveToFortyNine: 8.1,
+      aboveFortyNine: 5.5,
+      total: 8.8
+  },
+  {
+      trimester: "T3.2004",
+      fifteenToTwentyFour: 20.8,
+      twentyFiveToFortyNine: 8.2,
+      aboveFortyNine: 5.2,
+      total: 8.9
+  },
+  {
+      trimester: "T4.2004",
+      fifteenToTwentyFour: 20.6,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 5.2,
+      total: 8.9
+  },
+  {
+      trimester: "T1.2005",
+      fifteenToTwentyFour: 20.5,
+      twentyFiveToFortyNine: 8.1,
+      aboveFortyNine: 5,
+      total: 8.7
+  },
+  {
+      trimester: "T2.2005",
+      fifteenToTwentyFour: 20.3,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 4.9,
+      total: 8.8
+  },
+  {
+      trimester: "T3.2005",
+      fifteenToTwentyFour: 21.8,
+      twentyFiveToFortyNine: 8.2,
+      aboveFortyNine: 5.3,
+      total: 9
+  },
+  {
+      trimester: "T4.2005",
+      fifteenToTwentyFour: 21.5,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 5.4,
+      total: 9
+  },
+  {
+      trimester: "T1.2006",
+      fifteenToTwentyFour: 22.7,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 5.3,
+      total: 9.1
+  },
+  {
+      trimester: "T2.2006",
+      fifteenToTwentyFour: 22.3,
+      twentyFiveToFortyNine: 8,
+      aboveFortyNine: 5.5,
+      total: 8.9
+  },
+  {
+      trimester: "T3.2006",
+      fifteenToTwentyFour: 21.8,
+      twentyFiveToFortyNine: 8.1,
+      aboveFortyNine: 5.2,
+      total: 8.9
+  },
+  {
+      trimester: "T4.2006",
+      fifteenToTwentyFour: 21.6,
+      twentyFiveToFortyNine: 7.5,
+      aboveFortyNine: 5,
+      total: 8.4
+  },
+  {
+      trimester: "T1.2007",
+      fifteenToTwentyFour: 21,
+      twentyFiveToFortyNine: 7.6,
+      aboveFortyNine: 5.3,
+      total: 8.4
+  },
+  {
+      trimester: "T2.2007",
+      fifteenToTwentyFour: 19.8,
+      twentyFiveToFortyNine: 7.5,
+      aboveFortyNine: 4.8,
+      total: 8.1
+  },
+  {
+      trimester: "T3.2007",
+      fifteenToTwentyFour: 18.6,
+      twentyFiveToFortyNine: 7.5,
+      aboveFortyNine: 4.6,
+      total: 8
+  },
+  {
+      trimester: "T4.2007",
+      fifteenToTwentyFour: 18.6,
+      twentyFiveToFortyNine: 6.8,
+      aboveFortyNine: 4.3,
+      total: 7.5
+  },
+  {
+      trimester: "T1.2008",
+      fifteenToTwentyFour: 17.6,
+      twentyFiveToFortyNine: 6.6,
+      aboveFortyNine: 4.2,
+      total: 7.2
+  },
+  {
+      trimester: "T2.2008",
+      fifteenToTwentyFour: 18.5,
+      twentyFiveToFortyNine: 6.5,
+      aboveFortyNine: 4.7,
+      total: 7.3
+  },
+  {
+      trimester: "T3.2008",
+      fifteenToTwentyFour: 19.2,
+      twentyFiveToFortyNine: 6.7,
+      aboveFortyNine: 4.4,
+      total: 7.4
+  },
+  {
+      trimester: "T4.2008",
+      fifteenToTwentyFour: 20.6,
+      twentyFiveToFortyNine: 6.9,
+      aboveFortyNine: 4.4,
+      total: 7.7
+  },
+  {
+      trimester: "T1.2009",
+      fifteenToTwentyFour: 22.7,
+      twentyFiveToFortyNine: 7.6,
+      aboveFortyNine: 4.8,
+      total: 8.6
+  },
+  {
+      trimester: "T2.2009",
+      fifteenToTwentyFour: 23.9,
+      twentyFiveToFortyNine: 8.2,
+      aboveFortyNine: 5.4,
+      total: 9.2
+  },
+  {
+      trimester: "T3.2009",
+      fifteenToTwentyFour: 23.9,
+      twentyFiveToFortyNine: 8.2,
+      aboveFortyNine: 5.4,
+      total: 9.2
+  },
+  {
+      trimester: "T4.2009",
+      fifteenToTwentyFour: 23.9,
+      twentyFiveToFortyNine: 8.6,
+      aboveFortyNine: 5.9,
+      total: 9.5
+  },
+  {
+      trimester: "T1.2010",
+      fifteenToTwentyFour: 23.2,
+      twentyFiveToFortyNine: 8.5,
+      aboveFortyNine: 5.8,
+      total: 9.4
+  },
+  {
+      trimester: "T2.2010",
+      fifteenToTwentyFour: 23.5,
+      twentyFiveToFortyNine: 8.4,
+      aboveFortyNine: 5.6,
+      total: 9.3
+  },
+  {
+      trimester: "T3.2010",
+      fifteenToTwentyFour: 24,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 5.6,
+      total: 9.2,
+  },
+  {
+      trimester: "T4.2010",
+      fifteenToTwentyFour: 22.3,
+      twentyFiveToFortyNine: 8.5,
+      aboveFortyNine: 5.7,
+      total: 9.2
+  },
+  {
+      trimester: "T1.2011",
+      fifteenToTwentyFour: 23.1,
+      twentyFiveToFortyNine: 8.4,
+      aboveFortyNine: 5.6,
+      total: 9.1
+  },
+  {
+      trimester: "T2.2011",
+      fifteenToTwentyFour: 22.7,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 5.6,
+      total: 9.1
+  },
+  {
+      trimester: "T3.2011",
+      fifteenToTwentyFour: 22.1,
+      twentyFiveToFortyNine: 8.6,
+      aboveFortyNine: 5.8,
+      total: 9.2
+  },
+  {
+      trimester: "T4.2011",
+      fifteenToTwentyFour: 22.7,
+      twentyFiveToFortyNine: 8.8,
+      aboveFortyNine: 5.9,
+      total: 9.3
+  },
+  {
+      trimester: "T1.2012",
+      fifteenToTwentyFour: 22.9,
+      twentyFiveToFortyNine: 9,
+      aboveFortyNine: 5.8,
+      total: 9.5
+  },
+  {
+      trimester: "T2.2012",
+      fifteenToTwentyFour: 23.6,
+      twentyFiveToFortyNine: 9.1,
+      aboveFortyNine: 6.2,
+      total: 9.7
+  },
+  {
+      trimester: "T3.2012",
+      fifteenToTwentyFour: 24.7,
+      twentyFiveToFortyNine: 9.1,
+      aboveFortyNine: 6.1,
+      total: 9.8
+  },
+  {
+      trimester: "T4.2012",
+      fifteenToTwentyFour: 26.1,
+      twentyFiveToFortyNine: 9.2,
+      aboveFortyNine: 6.5,
+      total: 10.1
+  },
+  {
+      trimester: "T1.2013",
+      fifteenToTwentyFour: 25.7,
+      twentyFiveToFortyNine: 9.5,
+      aboveFortyNine: 6.7,
+      total: 10.3
+  },
+  {
+      trimester: "T2.2013",
+      fifteenToTwentyFour: 25.4,
+      twentyFiveToFortyNine: 9.7,
+      aboveFortyNine: 6.9,
+      total: 10.5
+  },
+  {
+      trimester: "T3.2013",
+      fifteenToTwentyFour: 24.7,
+      twentyFiveToFortyNine: 9.6,
+      aboveFortyNine: 6.9,
+      total: 10.3
+  },
+  {
+      trimester: "T4.2013",
+      fifteenToTwentyFour: 23.9,
+      twentyFiveToFortyNine: 9.6,
+      aboveFortyNine: 6.6,
+      total: 10.1
+  },
+  {
+      trimester: "T1.2014",
+      fifteenToTwentyFour: 23.8,
+      twentyFiveToFortyNine: 9.5,
+      aboveFortyNine: 7,
+      total: 10.1
+  },
+  {
+      trimester: "T2.2014",
+      fifteenToTwentyFour: 24,
+      twentyFiveToFortyNine: 9.6,
+      aboveFortyNine: 6.8,
+      total: 10.2,
+  },
+  {
+      trimester: "T3.2014",
+      fifteenToTwentyFour: 24.3,
+      twentyFiveToFortyNine: 9.8,
+      aboveFortyNine: 6.8,
+      total: 10.3
+  },
+  {
+      trimester: "T4.2014",
+      fifteenToTwentyFour: 24.7,
+      twentyFiveToFortyNine: 9.9,
+      aboveFortyNine: 7,
+      total: 10.4
+  },
+  {
+      trimester: "T1.2015",
+      fifteenToTwentyFour: 24.9,
+      twentyFiveToFortyNine: 9.8,
+      aboveFortyNine: 6.7,
+      total: 10.3
+  },
+  {
+      trimester: "T2.2015",
+      fifteenToTwentyFour: 24.6,
+      twentyFiveToFortyNine: 9.8,
+      aboveFortyNine: 7.3,
+      total: 10.5
+  },
+  {
+      trimester: "T3.2015",
+      fifteenToTwentyFour: 24.5,
+      twentyFiveToFortyNine: 9.8,
+      aboveFortyNine: 7.1,
+      total: 10.4
+  },
+  {
+      trimester: "T4.2015",
+      fifteenToTwentyFour: 24.5,
+      twentyFiveToFortyNine: 9.6,
+      aboveFortyNine: 6.9,
+      total: 10.2
+  },
+  {
+      trimester: "T1.2016",
+      fifteenToTwentyFour: 24.9,
+      twentyFiveToFortyNine: 9.6,
+      aboveFortyNine: 6.7,
+      total: 10.2
+  },
+  {
+      trimester: "T2.2016",
+      fifteenToTwentyFour: 24.4,
+      twentyFiveToFortyNine: 9.3,
+      aboveFortyNine: 6.8,
+      total: 10
+  },
+  {
+      trimester: "T3.2016",
+      fifteenToTwentyFour: 25.1,
+      twentyFiveToFortyNine: 9,
+      aboveFortyNine: 7.2,
+      total: 10
+  },
+  {
+      trimester: "T4.2016",
+      fifteenToTwentyFour: 23.9,
+      twentyFiveToFortyNine: 9.4,
+      aboveFortyNine: 6.9,
+      total: 10
+  },
+  {
+      trimester: "T1.2017",
+      fifteenToTwentyFour: 22.4,
+      twentyFiveToFortyNine: 9,
+      aboveFortyNine: 6.8,
+      total: 9.6
+  },
+  {
+      trimester: "T2.2017",
+      fifteenToTwentyFour: 23.3,
+      twentyFiveToFortyNine: 8.7,
+      aboveFortyNine: 6.5,
+      total: 9.4
+  },
+  {
+      trimester: "T3.2017",
+      fifteenToTwentyFour: 22.3,
+      twentyFiveToFortyNine: 9.2,
+      aboveFortyNine: 6.6,
+      total: 9.7
+  },
+  {
+      trimester: "T4.2017",
+      fifteenToTwentyFour: 21.3,
+      twentyFiveToFortyNine: 8.3,
+      aboveFortyNine: 6.4,
+      total: 8.9
+  },
+  {
+      trimester: "T1.2018",
+      fifteenToTwentyFour: 21.4,
+      twentyFiveToFortyNine: 8.6,
+      aboveFortyNine: 6.5,
+      total: 9.2
+  },
+  {
+      trimester: "T2.2018 (p)",
+      fifteenToTwentyFour: 20.8,
+      twentyFiveToFortyNine: 8.5,
+      aboveFortyNine: 6.5,
+      total: 9.1
+  }
+];
