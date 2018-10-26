5725ec7cb923c14ce3f0c6f81398abf88ba78446
Gabriele Corti <33316703+borntofrappe@users.noreply.github.com>
2018-10-26 22:59:37 +0200

    add styled components

--- React - Redux Winter Quiz/src/AppResult.js
+++ React - Redux Winter Quiz/src/AppResult.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import './css/AppResult.css';
 import SVGTwitter from './SVGTwitter';
 
 // import styled components
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
+const Result = styled.div`
+  text-align: center;
+`;
+const Proclamation = styled.h2`
+  font-size: 1.2rem;
+  margin: 0.8rem 0;
+`;
+const Score = styled.h1`
+  font-weight: 300;
+  margin: 1rem 0 2rem;
+`;
+const Link = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  font-family: inherit;
+  color: inherit;
+  padding: 0.5rem 0;
+  transition: all 0.2s ease-out;
 
+  svg {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+    margin: 0 0.5rem;
+    fill: inherit;
+    transition: all 0.2s ease-out;
+  }
+
+  &:hover svg {
+    background: #333;
+    fill: #fff;
+  }
+`;
 /*
-render a simple stateless component highlighting the score of the quiz
+render a stateless component highlighting the score of the quiz
 additionally detail a link to share the result on twitter 
 */
 const AppResult = (props) => {
@@ -16,17 +49,19 @@ const AppResult = (props) => {
    * number of toal questions
    */ 
   const { correct, total } = props;
+  // create the tweet detailing the text in the appropriate attribute
   const tweet = `https://twitter.com/intent/tweet?text=Answered%20correctly%20to%20${correct}%20questions%20out%20of%20${total}.%20Your%20turn.`;
   
+  // render a header describing the number of correct answers given, atop an anchor link to share the score on twitter
   return (
-    <div className="AppResult">
-      <h2>You have answered correctly to</h2>
-      <h1>{correct} questions out of {total}</h1>
+    <Result className="AppResult">
+      <Proclamation>You have answered correctly to</Proclamation>
+      <Score>{correct} questions out of {total}</Score>
 
-      <a href={tweet}>
+      <Link href={tweet}>
         Feel free to brag about it <SVGTwitter />
-      </a>
-    </div>
+      </Link>
+    </Result>
   );
 }
 
