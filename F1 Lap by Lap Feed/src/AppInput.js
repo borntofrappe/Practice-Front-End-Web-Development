737d5bf1b1eb7193ce3d5f85263791a601513081
gabrielecorti <borntofrappe@gmail.com>
2018-11-07 00:27:22 +0100

    style the application

--- F1 Lap by Lap Feed/src/AppInput.js
+++ F1 Lap by Lap Feed/src/AppInput.js
@@ -1,27 +1,88 @@
 import React from 'react';
-// import styled components and create a dashboard for the input element
+// import styled components and create a dashboard for the input elements
 import styled from 'styled-components';
 
+// position the input elements and the button side by side
 const Form = styled.form`
   width: 100%;
   display: flex;
   align-items: center;
-  padding: 1.5rem 1.5rem 3rem;
+  padding: 1rem 1.5rem 2.5rem;
   background: #e10600;
+  box-shadow: 0 1px 5px #e10600;
   border-radius: 0 0 40px 40px;
+  margin-bottom: 1.5rem;
 `;
+// stretch the input elements to take the majority of the horizontal space
+// display the input elements horizontally
 const Inputs = styled.div`
+flex-grow: 1;
   display: flex;
   flex-direction: column;
-  flex-grow: 1;
+`;
+// display the labels boldy on the red background
+const Label = styled.label`
+  font-family: "KoHo", sans-serif;
+  font-size: 1.4rem;
+  color: #fff;
+  margin: 0.5rem 0;
+  text-transform: uppercase;
 `;
 const Input = styled.input`
-  // width: 100%;
-  // flex-grow: 1;
+  background: #fff;
+  border: none;
+  padding: 1rem;
+  margin: 0.5rem 0;
+  border-radius: 5px;
+  font-family: inherit;
+  font-size: 1.1rem;
 `;
+// include a round circle and an arrow pointing downwards through pseudo elements
 const Button = styled.button`
-  // width: 100%;
-  margin: 1rem;
+  margin: 0 1rem;
+  width: 52px;
+  height: 52px;
+  background: #fff;
+  border: none;
+  border-radius: 50%;
+  position: relative;
+  font-size: 0;
+
+  &:before {
+    position: absolute;
+    content: '';
+    top: 25%;
+    height: 40%;
+    width: 4px;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #e10600;
+    transition: all 0.2s ease-out;
+  }
+  &:after {
+    position: absolute;
+    content: '';
+    top: 65%;
+    left: 50%;
+    transform: translateX(-50%);
+    border-top: 10px solid #e10600;
+    border-right: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    border-left: 10px solid transparent;
+    transition: all 0.2s ease-out;
+  }
+  &:hover:before {
+    height: 35%;
+  }
+  &:hover:after {
+    top: 60%;
+  }
+  &:active:before {
+    height: 40%;
+  }
+  &:active:after {
+    top: 65%;
+  }
 `;
 
 // create a stateless component rendering simply an input element and a button element
@@ -31,15 +92,15 @@ const AppInput = (props) => {
     <Form className="AppInput" onSubmit={handleSubmit}>
       <Inputs>
         <>
-          <label>Title</label>
+          <Label>Title</Label>
           <Input type="text" id="title" value={title} onInput={handleInput} placeholder="RACE DAY" />
         </>
         <>
-          <label>Subtitle</label>
+          <Label>Subtitle</Label>
           <Input type="text" id="subtitle" value={subtitle} onInput={handleInput} placeholder="12:45" />
         </>
         <>
-          <label>Update*</label>
+          <Label>Update*</Label>
           <Input required type="text" id="update" value={update} onInput={handleInput} placeholder="The race is about to start" />
         </>
       </Inputs>
--- F1 Lap by Lap Feed/src/AppOutput.js
+++ F1 Lap by Lap Feed/src/AppOutput.js
@@ -1,6 +1,46 @@
 import React from 'react';
-// import './css/AppOutput.css';
+import styled from 'styled-components';
 
+const Section = styled.section`
+  border-left: 2px dotted #33333322;
+  padding: 2rem 1rem 3rem;
+  line-height: 2;
+  position: relative;
+  margin-left: 0.5rem;
+
+  &:after {
+    position: absolute;
+    content: "";
+    top: 0;
+    left: 0;
+    transform: translate(-50%, -50%);
+    width: 0.7rem;
+    height: 0.7rem;
+    border-radius: 50%;
+    background: #fff;
+    border: 2px solid #e10600;
+  }
+  &:before {
+    position: absolute;
+    content: "";
+    top: 0;
+    left: 3%;
+    width: 92%;
+    height: 2rem;
+    border-top: #33333388 0.1rem solid;
+    border-right: #33333388 0.1rem solid;
+    border-top-right-radius: 20px;
+  }
+`;
+const Title = styled.h2`
+  font-size: 2rem;
+`;
+const Subtitle = styled.h3`
+  font-size: 1.65rem;
+`;
+const Update = styled.p`
+  font-size: 1.1rem;
+`;
 // in a stateless component detail each update in a section
 // currently considering only paragraph elements
 const AppOutput = (props) => {
@@ -9,17 +49,17 @@ const AppOutput = (props) => {
     const { title, subtitle, update: up } = update;
 
     return (
-      <section key={key}>
+      <Section key={key}>
         {
           title &&
-          <h2>{title}</h2>
+          <Title>{title}</Title>
         }
         {
           subtitle &&
-          <h3>{subtitle}</h3>
+          <Subtitle>{subtitle}</Subtitle>
         }
-        <p>{up}</p>
-      </section>
+        <Update>{up}</Update>
+      </Section>
     );
   });
   return (
