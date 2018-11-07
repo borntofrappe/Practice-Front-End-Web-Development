e7978146f9251287533614a794eaf98ae4486701
gabrielecorti <borntofrappe@gmail.com>
2018-11-08 00:47:42 +0100

    document code

--- D3 Scatter Plot Practice/script.js
+++ D3 Scatter Plot Practice/script.js
@@ -7,7 +7,7 @@
 
 the first to identify the data point, the other two to detail the x and y coordinate respectively
 */
-// create a function returning a random value between 1 and a max value
+// create a function returning a random value between 1 and a maximum value
 const maxValue = 100;
 const randomInt = () => Math.ceil(Math.random() * maxValue);
 
@@ -22,6 +22,7 @@ for (let i = 0; i < datasetLength; i += 1) {
   let x = randomInt();
   let y = randomInt();
 
+  // in the middle of the array start incrementing the values as to show an uneven distribution in the scatter plot
   if (i >= datasetLength / 2) {
     x = (x < 50) ? x + 50 : x;
     y = (y < 50) ? y + 50 : y;
@@ -36,8 +37,15 @@ for (let i = 0; i < datasetLength; i += 1) {
 // plot a scatter plot based on the dataset
 // ! include noticeable margin at the top and to the right, as to later include the histograms
 
+// SELECT
 const container = d3
   .select('.container');
+
+// HTML ELEMENTS
+container
+  .append('h1')
+  .attr('class', 'title')
+  .text('Scatter Plot Practice');
 
 const tooltip = container
   .append('div')
@@ -46,11 +54,7 @@ const tooltip = container
   .style('position', 'absolute')
   .style('pointer-events', 'none');
 
-container
-  .append('h1')
-  .attr('class', 'title')
-  .text('Scatter Plot Practice');
-
+// SVG FRAME
 const margin = {
   top: 100,
   right: 100,
@@ -68,6 +72,7 @@ const containerFrame = container
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 // LABELS
+// position arbitrary labels to the left and to the bottom of the scatter plot
 containerFrame
   .append('text')
   .attr('x', -margin.left)
@@ -84,8 +89,9 @@ containerFrame
   .attr('text-anchor', 'middle')
   .attr('alignment-baseline', 'after-edge')
   .text('Quantity');
+
 // SCALES
-// for the scatter plot detail linear scales
+// for the scatter plot detail linear scales, based on the maximum value and the width/height
 const xScale = d3
   .scaleLinear()
   .domain([0, maxValue])
@@ -96,11 +102,13 @@ const yScale = d3
   .domain([0, maxValue])
   .range([height, 0]);
 
+// reduce the number of ticks shown
 const xAxis = d3
   .axisBottom(xScale)
   .tickSize(0)
   .ticks(4)
   .tickPadding(10)
+  // hide the first tick to show the origin only on the y axis
   .tickFormat(d => (d !== 0 ? d : ''));
 
 const yAxis = d3
@@ -120,6 +128,7 @@ containerFrame
   .attr('class', 'y axis')
   .call(yAxis);
 
+// add grid lines stemming from the ticks
 d3.select('.x')
   .selectAll('g')
   .append('path')
@@ -131,7 +140,7 @@ d3.select('.y')
   .append('path')
   .attr('d', `M 0 0 h ${width}`);
 
-
+// reduce the weight of the axes
 d3.selectAll('.axis')
   .selectAll('path')
   .attr('stroke', '#33333322')
@@ -140,8 +149,9 @@ d3.selectAll('.axis')
 
 
 // add circles for the scatter plot
-const circlesDuration = 200;
-const circlesDelay = 1000;
+// separating the selection according to the update/enter/exit loop
+// ! this is planned for a future update
+
 const update = containerFrame
   .selectAll('circle')
   .data(dataset, d => d.key);
@@ -149,8 +159,12 @@ const update = containerFrame
 const enter = update
   .enter();
 
-const exit = update
-  .exit();
+// const exit = update
+//   .exit();
+
+// for new elements, animate the circles into view following two control values
+const circlesDuration = 200;
+const circlesDelay = 1000;
 
 enter
   .append('circle')
@@ -161,30 +175,36 @@ enter
   .duration(circlesDuration)
   .delay((d, i) => i / dataset.length * circlesDelay)
   .attr('r', 4)
-  .attr('fill', '#33333377');
+  .attr('fill', '#ccc');
 
-exit
-  .remove();
+// exit
+//   .remove();
 
 
-// compute the histograms on the basis of the dataset x and y values
+// compute the histograms on the basis of the dataset and its x and y values
 const histogram = d3
   .histogram()
+  // limit the number of bins to the number of ticks for the axes
+  // ! TODO: read the docs to have this number match the exact number of ticks and avoid magic numbers
   .thresholds(5);
 
+// build the histograms
+// ! each histogram is an array of arrays; in these last elements, the data points are separated according to value
 const xHistogram = histogram(dataset.map(data => data.x));
 const yHistogram = histogram(dataset.map(data => data.y));
-const histogramPadding = 2;
 
-// create scales based on the input data and available space (allocated by the margin.top and margin.right values)
+// add a simple variable to include whitespace around the rectangle elements
+const rectMargin = 2;
+
+// create a scale based on the input data and available space (allocated by the margin.top value)
 // input data in the form of the length of each histogram bin
 const xHistogramScale = d3
   .scaleLinear()
   .domain([0, d3.max(xHistogram, d => d.length)])
   .range([0, margin.top]);
 
-
-// based on the histogram, plot rectangle elements atop and to the right of the scatter plot
+// based on the histogram, plot rectangle elements atop the scatter plot
+// define variables for the transition (triggered as the transition for the circle elements come to a close)
 const rectDuration = 200;
 const rectDelay = 800;
 containerFrame
@@ -194,16 +214,15 @@ containerFrame
   .append('rect')
   .attr('class', 'x')
   .attr('x', (d, i) => width / xHistogram.length * i)
-  .attr('width', width / xHistogram.length - histogramPadding)
+  .attr('width', width / xHistogram.length - rectMargin)
   .transition()
   .duration(rectDuration)
   .delay((d, i) => i / xHistogram.length * rectDelay + circlesDelay)
   .attr('y', d => 0 - xHistogramScale(d.length))
   .attr('height', d => xHistogramScale(d.length))
-  .attr('stroke', 'none')
-  .attr('fill', '#333');
+  .attr('stroke', 'none');
 
-// repeat for the y axis, but considering margin.right
+// repeat for the y axis, but considering margin.right and the right side of the scatter plot
 const yHistogramScale = d3
   .scaleLinear()
   .domain([0, d3.max(yHistogram, d => d.length)])
@@ -217,23 +236,24 @@ containerFrame
   .append('rect')
   .attr('class', 'y')
   .attr('y', (d, i) => height - height / yHistogram.length * (i + 1))
-  .attr('height', height / yHistogram.length - histogramPadding)
+  .attr('height', height / yHistogram.length - rectMargin)
   .attr('x', width)
   .transition()
   .duration(rectDuration)
   .delay((d, i) => (xHistogram.length - i) / xHistogram.length * rectDelay + circlesDelay + rectDelay)
   .attr('width', d => yHistogramScale(d.length))
-  .attr('stroke', 'none')
-  .attr('fill', '#333');
+  .attr('stroke', 'none');
 
+// select all rectangles and add a bit of interactivity increasing the opacity on hover
+// ! detail also the number of points of each bin through the tooltip
 containerFrame
   .selectAll('rect')
-  .attr('opacity', 0.4)
+  .attr('fill', '#aaa')
   .on('mouseenter', function (d) {
     d3
       .select(this)
       .transition()
-      .attr('opacity', 0.7);
+      .attr('fill', '#777');
 
     tooltip
       .append('p')
@@ -250,7 +270,7 @@ containerFrame
     d3
       .select(this)
       .transition()
-      .attr('opacity', 0.4);
+      .attr('fill', '#aaa');
 
 
     tooltip
