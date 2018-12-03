857b34c609a3412018396fb51b1a6a6bd25c482f
gabrielecorti <borntofrappe@gmail.com>
2018-12-03 23:31:26 +0100

    add working files

--- D3 Slider/script.js
+++ D3 Slider/script.js
@@ -1,23 +1,24 @@
 /* globals d3 */
-/*
-! still much to do, the goal is to have a slider which is positioned exactly where the cursor lays it and a slider which can be also resized
-*/
 // global variables
 const globals = {
-  max: 100,
+  // number of data points
+  points: 100,
+  // boolean for the draggable slider
   isSelected: false,
-  begin: 10,
-  width: 10
+  // where the slider ought to begin and its width
+  begin: 40,
+  width: 10,
+  // color used in the visualization
+  color: '#F54780'
 };
-// for the data create an arbitrary set of max numbers, potentially increasing in value
+// for the data create an arbitrary set of #globals.points numbers, gradually increasing in value
 const data = [];
-
-for (let i = 0; i < globals.max; i += 1) {
+for (let i = 0; i < globals.points; i += 1) {
   data.push(i / 2 + Math.floor(Math.random() * 10));
 }
 
 
-// introductory elements for the entire visualization
+// HTML elements
 const container = d3
   .select('.container');
 
@@ -29,20 +30,26 @@ container
   .append('p')
   .text('Enhancing a line chart with a draggable element');
 
-// paragraph detailing the average value of the selected area (later to be changed as the selection area changes)
-const paragraphSelection = container
-  .append('p');
-// paragraph updated through a function (later called when the slider is modified in its horizontal position)
-function updateSelection(begin = 10) {
-  globals.begin = begin;
-  const selection = [...data.slice(globals.begin, globals.begin + globals.width)];
+
+// paragraph detailing the average value of the selected area
+// ! update the text of the strong element when the slider is dragged
+const selectionElement = container
+  .append('p')
+  .text('Average in the selected area: ')
+  .append('strong');
+
+// the element updated with a function, computing the average of the data points falling in the selected area
+function updateSelection(beginSelection = 10, widthSelection = 10) {
+  // find the data points in the selection, compute the average and include it in the selected element
+  const selection = [...data.slice(beginSelection, beginSelection + widthSelection)];
   const averageSelection = d3.sum(selection) / selection.length;
   const formatSelection = d3.format('.4');
 
-  paragraphSelection
-    .html(`Average in the selected area: ${formatSelection(averageSelection)}`);
+  selectionElement
+    .text(formatSelection(averageSelection));
 }
-updateSelection();
+// immediately call the function with the values specified in the globals object
+updateSelection(globals.begin, globals.width);
 
 // SVG FRAME
 const margin = {
@@ -55,24 +62,33 @@ const margin = {
 const width = 900 - (margin.left + margin.right);
 const height = 400 - (margin.top + margin.bottom);
 
-const containerFrame = container
+const containerSVG = container
   .append('svg')
-  .attr('viewBox', `0 0 ${width + (margin.left + margin.right)} ${height + (margin.top + margin.bottom)}`)
+  .attr('viewBox', `0 0 ${width + (margin.left + margin.right)} ${height + (margin.top + margin.bottom)}`);
+
+const containerFrame = containerSVG
   .append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+// ! for the slider, include a defs block in which to detail a linear gradient
+const containerDefs = containerSVG
+  .insert('defs');
+
 // SCALES
+// horizontal scale based on the number of observations
 const xScale = d3
   .scaleLinear()
-  .domain([0, globals.max - 1])
+  .domain([0, globals.points - 1])
   .range([0, width]);
 
+// vertical scale based on the actual values
 const yScale = d3
   .scaleLinear()
   .domain([0, d3.max(data)])
   .range([height, 0]);
 
 // AXES
+// don't show any label on the horizontal axis
 const xAxis = d3
   .axisBottom(xScale)
   .tickSize(0)
@@ -83,9 +99,10 @@ containerFrame
   .attr('transform', `translate(0, ${height})`)
   .call(xAxis);
 
+// limit the number of ticks on the vertical axis
 const yAxis = d3
   .axisLeft(yScale)
-  .ticks(7)
+  .ticks(5)
   .tickSize(0)
   .tickPadding(10)
   .tickFormat(d => (d !== 0 ? d : ''));
@@ -114,46 +131,56 @@ containerFrame
   .append('path')
   .attr('d', line(data))
   .attr('fill', 'none')
-  .attr('stroke', '#B89C1AAA')
-  .attr('stroke-width', '2px');
+  .attr('stroke', `${globals.color}aa`)
+  .attr('stroke-width', '3px');
 
 // path using the area function
 containerFrame
   .append('path')
   .attr('d', area(data))
-  .attr('fill', '#B89C1A22')
+  .attr('fill', `${globals.color}11`)
   .attr('stroke', 'none');
 
 
-// group nesting the rectangle used for the slider
-// detail a clip to crop out the stroke atop and below the rectangle
-const clip = containerFrame
-  .append('clipPath')
-  .attr('id', 'clip');
-
-clip
-  .append('rect')
-  .attr('x', 0)
-  .attr('y', 0)
-  .attr('width', width)
-  .attr('height', height);
-
+// group nesting the element(s) used for the slider
 const containerSlider = containerFrame
-  .append('g')
-  .attr('clip-path', 'url(#clip)');
+  .append('g');
 
-// detail a rectangle covering the prescribed selected area
-// vertically going past the parent element (ultimately cropped with the clip path to show only horizontal elements)
-const rectSlider = containerSlider
-  .append('rect')
-  .attr('x', xScale(globals.begin))
-  .attr('y', -4)
-  .attr('width', xScale(globals.width))
-  .attr('height', height + 8)
-  .attr('fill', '#B89C1A44')
-  .attr('stroke', '#B89C1A')
-  .attr('stroke-width', '4px');
+// detail a linear gradient applied on a section of a rectangle used as overlay
+// this linear gradient is moved programmatically depending on user input
 
+const containerGradient = containerDefs
+  .append('linearGradient')
+  .attr('id', 'gradient');
+
+
+function updateGradient(beginGradient = 10, widthGradient = 10) {
+  const dataGradient = [
+    { offset: '0%', color: 'transparent' },
+    { offset: `${beginGradient}%`, color: 'transparent' },
+    { offset: `${beginGradient}%`, color: `${globals.color}55` },
+    { offset: `${beginGradient + widthGradient}%`, color: `${globals.color}55` },
+    { offset: `${beginGradient + widthGradient}%`, color: 'transparent' },
+  ];
+
+  const update = containerGradient
+    .selectAll('stop')
+    .data(dataGradient);
+
+  const enter = update
+    .enter();
+
+  enter
+    .append('stop')
+    .attr('offset', d => d.offset)
+    .attr('stop-color', d => d.color);
+
+  update
+    .attr('offset', d => d.offset)
+    .attr('stop-color', d => d.color);
+}
+
+updateGradient(globals.begin, globals.width);
 
 // handling the slider with an overlay covering the entire viz
 const overlay = containerSlider
@@ -162,7 +189,7 @@ const overlay = containerSlider
   .attr('y', 0)
   .attr('width', width)
   .attr('height', height)
-  .attr('fill', 'transparent')
+  .attr('fill', 'url(#gradient)')
   .style('cursor', 'move');
 
 // when the cursor is down set the boolean to track a selection to true
@@ -181,19 +208,15 @@ overlay
 
 // listen for a mousemove event
 overlay
-  .on('mousemove', () => {
+  .on('mousemove', function () {
     // if the cursor is down on the visualization proceed finding the horizontal coordinate and updating the slider's position
     if (globals.isSelected) {
-      const svg = document.querySelector('svg');
-      const rect = svg.getBoundingClientRect();
-      const xCoor = d3.event.pageX - rect.x;
-
-      rectSlider
-        .attr('x', xCoor);
-
-      // modify the text in the pargraph selection to match the new set of data
-      // based on the value retrieved from the scale, from the actual coordinate
-      const xCoorScale = Math.floor(xScale.invert(xCoor));
-      updateSelection(xCoorScale);
+      // find the horizontal coordinate
+      const mouse = d3.mouse(this);
+      const [xCoor] = mouse;
+      // compute the distance from the left edge
+      const x = Math.floor(xCoor / width * 100);
+      updateSelection(x);
+      updateGradient(x);
     }
   });
