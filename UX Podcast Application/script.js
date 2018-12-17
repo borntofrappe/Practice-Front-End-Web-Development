dc1e87a6df27061d0df7f01c94c359759289f2f9
gabrielecorti <borntofrappe@gmail.com>
2018-12-17 23:55:18 +0100

    restructure function to animate the rotation with js

--- UX Podcast Application/script.js
+++ UX Podcast Application/script.js
@@ -4,31 +4,39 @@
 const vinyl = document.querySelector('.player__vinyl');
 
 // button for the play/ pause animation & functionality
-// including the nested svg and audio elements
+// including the nested audio element
 const toggleButton = document.querySelector('.controls--toggle');
-const toggleSVG = toggleButton.querySelector('svg');
 const audio = toggleButton.querySelector('audio');
+
+// button to mute the audio
+const muteButton = document.querySelector('.controls--mute');
+
+// button to change the speed rate, alongside an array detailing possible values
+const speedButton = document.querySelector('.controls--speed');
+const speedOptions = [1, 1.5, 1.75, 2, 2.5, 3];
+
+// button to stop the audio
+const stopButton = document.querySelector('.controls--stop');
 
 // div for the progress bar
 const episodeProgress = document.querySelector('.episode--progress');
 // span for the current time stamp
 const timingCurrent = document.querySelector('.timing--current');
 
-// global variable(s) to keep track of the play/pause alternative and adjust the progress bar according to the total length of the podcast
-// ! this value, much alike those in the DOM, is hard coded, but in a working application it would be retrieved from the RSS feed
+// variable(s) to keep track of the play/pause alternative, as well the mute/ un-muted pair and to adjust the progress bar according to the total length of the podcast
 let isPlaying = false;
 let intervalID = 0;
-const total = 216552;
+const episodeDuration = 4152;
+
 
 /* function playing the audio, and achieving the following
-- change the reference of the <use /> element for the SVG, to pause
 - play the audio
 - set up an intervl used to update the current time stamp and the progress bar
 ! audio.currentTime allows to retrieve the number of seconds since the beginning of the audio
 */
 const playAudio = () => {
-  toggleSVG.innerHTML = '<use href="#pause"/>';
   audio.play();
+
   intervalID = setInterval(() => {
     const { currentTime } = audio;
 
@@ -36,6 +44,10 @@ const playAudio = () => {
     let currentSeconds = Math.floor(currentTime);
     let currentMinutes = Math.floor(currentSeconds / 60);
     const currentHours = Math.floor(currentMinutes / 60);
+    // using the unmodified value for currentSeconds, update the appearance of teh application (the vinyl rotation and the progress bar)
+    vinyl.style.transition = 'transform 1s linear';
+    vinyl.style.transform = `rotate(${currentSeconds * 6}deg)`;
+    episodeProgress.style.background = `linear-gradient(to right, green, green ${currentSeconds / episodeDuration * 100}%, white ${currentSeconds / episodeDuration * 100}%)`;
     if (currentMinutes > 0) {
       currentSeconds -= 60 * currentMinutes;
     }
@@ -44,15 +56,11 @@ const playAudio = () => {
     }
     // display the hour/minute/second as zero-padded integers
     timingCurrent.textContent = `${currentHours < 10 ? `0${currentHours}` : currentHours}:${currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes}:${currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds}`;
-
-    // update the progress bar
-    episodeProgress.style.background = `linear-gradient(to right, #006400, #006400 ${currentTime / total * 100}%, white ${currentTime / total * 100}%)`;
   }, 1000);
 };
 
 // function pausing the audio
 const pauseAudio = () => {
-  toggleSVG.innerHTML = '<use href="#play"/>';
   audio.pause();
   // ! clear the currently-ongoing interval
   clearInterval(intervalID);
@@ -60,14 +68,20 @@ const pauseAudio = () => {
 
 // function called in response to a click event on the toggle button
 // toggle the boolean and call the play/pause function according to its value
-// ! add/remove a class of .playing to animate the vinyl
+// detail the correct SVG in the toggleButton
 const toggleEpisode = () => {
   isPlaying = !isPlaying;
   if (isPlaying) {
-    vinyl.classList.add('playing');
+    toggleButton.innerHTML = `
+    <svg width="50" height="50">
+      <use href = "#pause" />
+    </svg>`;
     playAudio();
   } else {
-    vinyl.classList.remove('playing');
+    toggleButton.innerHTML = `
+    <svg width="50" height="50">
+      <use href = "#play" />
+    </svg>`;
     pauseAudio();
   }
 };
