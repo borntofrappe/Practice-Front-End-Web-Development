3f3fbd81df5073b156ab6998940de56336c2d400
gabrielecorti <borntofrappe@gmail.com>
2019-01-09 22:23:53 +0100

    add the working files

--- CodepenChallenge Peach/script.js
+++ CodepenChallenge Peach/script.js
@@ -1,6 +1,18 @@
-// target the SVG and the heading
-const container = document.querySelector('svg');
+// target the SVG and the pin right next to it
+const containerSlices = document.querySelector('g#slices');
+const pin = document.querySelector('svg#pin');
+
+// immediately add simple dots around the wheel
+for (let i = 0; i < 48; i += 1) {
+  const transform = `rotate(${360 / 48 * i}), translate(0 -49.5), rotate(${-360 / 48 * i})`;
+  const dot = `<circle r="0.5" cx="50" cy="50" fill="currentColor" transform="${transform}"/>`;
+  containerSlices.innerHTML += dot;
+}
+
+// target the heading and the button
 const heading = document.querySelector('h1');
+const spinButton = document.querySelector('button');
+
 // variable updated for the timeout
 let timeoutID = 0;
 
@@ -25,11 +37,18 @@ const suspicious = [
   }
 ];
 
+// add a random fill color to the circle behind the slices
+let randomFill = '';
+for (let i = 0; i < 6; i += 1) {
+  randomFill += randomHex();
+}
+document.querySelector('svg circle#slice').style.fill = randomFill;
+
 // create the slices, 24 in total, using a bit of trigonometry to compute the appropriate arc coordinates
 for (let i = 360; i > 0; i -= 15) {
   // values for the path element
-  const xCoor = 50 + Math.sin(i * Math.PI / 180) * 50;
-  const yCoor = 50 - Math.cos(i * Math.PI / 180) * 50;
+  const xCoor = 50 + Math.sin(i * Math.PI / 180) * 47;
+  const yCoor = 50 - Math.cos(i * Math.PI / 180) * 47;
   const flags = i > 180 ? '0 1 1' : '0 0 1';
 
   // initialize a variable for the fill color
@@ -48,15 +67,22 @@ for (let i = 360; i > 0; i -= 15) {
 
   // create the path element and append it to the SVG container
   const path = `
-    <path d="M 50 50 L 50 0 A 50 50 ${flags} ${xCoor} ${yCoor}" fill=#${fill} />
+    <path d="M 50 50 L 50 3 A 47 47 ${flags} ${xCoor} ${yCoor}" fill=#${fill} />
   `;
-  container.innerHTML += path;
+  containerSlices.innerHTML += path;
 }
 
 // function spinning the wheel
 function spinWheel() {
+  // remove the event listener from the button and the wheel, to avoid running the function twice at the same time
+  spinButton.removeEventListener('click', spinWheel);
+  pin.removeEventListener('click', spinWheel);
+
   // immediately hide the heading showing the matching color
   heading.classList.add('isHidden');
+  // add a class to the pin and the button to show how they should not be clicked
+  pin.classList.add('isSpinning');
+  spinButton.classList.add('isSpinning');
 
   // create variables for the duration of the rotation, as whell as the number of rotations achieved by the wheel
   const randomDuration = randomInt(4, 10);
@@ -65,23 +91,39 @@ function spinWheel() {
   const randomSuspect = randomInt(0, 3);
 
   // apply the transition and the transform properties
-  container.style.transition = `transform ${randomDuration}s ease-out`;
+  containerSlices.style.transformOrigin = '50%';
+  containerSlices.style.transition = `transform ${randomDuration}s ease-out`;
   /* for the rotation, beside an arbitrary x360 rotation, remember to
   - add 90 to match the position of the arrow (to the very right of the wheel)
   - subtract the rotation of the slices
   - add up to a slice as to have the arrow point within the slice's boundaries
   */
-  container.style.transform = `translateY(-50%) rotate(${randomRotate * 360 - suspicious[randomSuspect].rotation + 90 + randomInt(0, 360 / 24)}deg)`;
+  containerSlices.style.transform = `rotate(${randomRotate * 360 - suspicious[randomSuspect].rotation + 90 + randomInt(0, 360 / 24)}deg)`;
+
+  pin.style.animation = `pinWheel ${randomDuration / 10}s 10 ease-in-out`;
 
   // after the time allocated for the rotation show the heading with the "random" color, update the custom property with its value
   timeoutID = setTimeout(() => {
     heading.textContent = `#${suspicious[randomSuspect].color}`;
     heading.classList.remove('isHidden');
+    pin.style.animation = '';
     document.documentElement.style.setProperty('--color-theme', `#${suspicious[randomSuspect].color}`);
+
+    // remove the class on the pin and button showing the forbidden cursor
+    pin.classList.remove('isSpinning');
+    spinButton.classList.remove('isSpinning');
+
+    // reset the event listener on the button and the pin
+    spinButton.addEventListener('click', spinWheel);
+    pin.addEventListener('click', spinWheel);
+
+    // clear the interval and set the boolean back to false
     clearInterval(timeoutID);
   }, randomDuration * 1000);
 }
 
 // attach a click event listener on the button, at which point call the spinWheel function
-const spinButton = document.querySelector('button');
 spinButton.addEventListener('click', spinWheel);
+
+// call the same function when pressing the pin
+pin.addEventListener('click', spinWheel);
