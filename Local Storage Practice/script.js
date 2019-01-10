2ed06454e89607aed2e33cfa119232986c8b5de3
gabrielecorti <borntofrappe@gmail.com>
2019-01-11 00:00:37 +0100

    add working  local storage

--- Local Storage Practice/script.js
+++ Local Storage Practice/script.js
@@ -1,8 +1,16 @@
 // target the form element and the unordered list
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
+let initialItems = JSON.parse(localStorage.getItem('items')) || ['item'];
 
-const handleSubmit = (e) => {
+function populateList(items, list) {
+  list.innerHTML = items.map(item => `<li>${item}<svg><use href="#delete-icon"/></svg></li>`);
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+populateList(initialItems, ul);
+
+function handleSubmit(e) {
   // prevent the default behavior which refreshes the page
   e.preventDefault();
 
@@ -10,15 +18,15 @@ const handleSubmit = (e) => {
   const input = e.target.querySelector('input[type="text"]');
   const { value } = input;
 
-  // if something is included in the input, display it in the unordered list
+  // if something is included in the input, add the text to the items array and call the populate method
   if (value) {
-    // beside the text, add an icon through the specified svg element
-    const icon = '<svg><use href="#delete-icon" /></svg>';
-    const li = `<li>${value}${icon}</li>`;
-    ul.innerHTML += li;
-    input.value = '';
+    initialItems.push(value);
+    populateList(initialItems, ul);
   }
-};
+  // applied on a form, the reset() method allows to clear any input element
+  // ! in order to use this and have it refer to the element, you need a non-arrow function
+  this.reset();
+}
 
 // listen for a submit event on the form, at which point call a funtion to take the text input and display it in the unordered list
 form.addEventListener('submit', handleSubmit);
@@ -28,12 +36,13 @@ const handleClick = (e) => {
   // if the target of the click is a list item, remove said list item from the unordered list
   const { target } = e;
   const { tagName } = target;
+
   if (tagName === 'LI') {
-    // remove the selected list item by selecting all list items in the unordered list, filter the selected one out and re-render the unordered list
+    // remove the list item according to its position
     const listItems = ul.querySelectorAll('li');
-    const listItemsFilter = [...listItems].filter(listItem => listItem !== target);
-    // you can retrieve the HTML structure of the list items using the outerHTML method
-    ul.innerHTML = listItemsFilter.map(listItem => listItem.outerHTML).join('');
+    const listItemPosition = [...listItems].indexOf(target);
+    initialItems = [...initialItems.slice(0, listItemPosition), ...initialItems.slice(listItemPosition + 1)];
+    populateList(initialItems, ul);
   }
 };
 
