b61bba4c03e5eecb33bd0f2fe0e76b26e6aee0d9
gabrielecorti <borntofrappe@gmail.com>
2019-01-14 00:27:45 +0100

    read from the textarea element and with prescribed settings

--- JS30 Speech Synthesis API/script.js
+++ JS30 Speech Synthesis API/script.js
@@ -1,44 +1,79 @@
 // retrieve a reference to the speechSynthesis API
-const { speechSynthesis } = window;
-// retrieve a reference to the button element
-const buttonSpeak = document.querySelector('button#speak');
+const synth = window.speechSynthesis;
 
+// store a reference to the button and input elements
+const buttonSpeak = document.querySelector('button#feature--speak');
+const inputRanges = document.querySelectorAll('input[type="range"]');
 
+// global variables:
+// - fallback, a sting passed to the synthesizer if the textarea element has no value
+// - isMouseInRange, to modify the options of the inputs of type range only when the mouse is down on them (and later moved)
+// - options, to alter the settings of the voice
+
+const fallback = 'Write something here, hit the mic and behold, the program talks back!';
+let isMouseInRange = false;
 // initialize  variable to keep track of the voice and other options
 const options = {
-  voices: '',
-  fallback: 'These voices. They\'re outside. Where I must be.'
+  voice: '',
+  pitch: 1,
+  rate: 1
+
 };
 
-// function called when clicking the speak button (and when voices are readily available)
-// ! attach an event listener on the button only when the voices from the speechSynthesis object are readily available
+
+// function called in response to the mousemove event on the input elements
+function handleMove(e) {
+  // pre-emptively exit the function if the arbitrary boolean is set to false
+  if (!isMouseInRange) {
+    return false;
+  }
+
+  // retrieve the name and value of the input of type range
+  const { target } = e;
+  const { name, value } = target;
+
+  // change the option bearing the same name of the input element with the obtained value
+  options[name] = value;
+}
+// toggle the boolean when the mouse is down/up on the input elements
+inputRanges.forEach(inputRange => inputRange.addEventListener('mousedown', () => isMouseInRange = true));
+inputRanges.forEach(inputRange => inputRange.addEventListener('mouseup', () => isMouseInRange = false));
+// call the function updating the options when moving the cursor on the input elements
+inputRanges.forEach(inputRange => inputRange.addEventListener('mousemove', handleMove));
+
+
+// function called in response to a click on the selected button
+// ! the event listener is attached on the button only when the voices from the speechSynthesis object are readily available
 function speak() {
   // extract the text from the textarea element
   const { value } = document.querySelector('textarea');
 
-  // create an instance of SpeechSynthesisUtterance and use the .speak() method available on the speechSynthesis object
-  // use the value of the textarea, or the fallback value detailed in the options if this proves to be an empty string
-  const speech = value || options.fallback;
+  // create an instance of SpeechSynthesisUtterance and use the .speak() method available on the synth object
+  // use the value of the textarea, or the fallback value
+  const speech = value || fallback;
 
   const utterance = new SpeechSynthesisUtterance(speech);
+  // set the options specified in the options object
   utterance.voice = options.voice;
-  speechSynthesis.speak(utterance);
+  utterance.rate = options.rate;
+  utterance.pitch = options.pitch;
+
+  synth.speak(utterance);
 }
 
 // funciton called in response to the _onvoiceschanged_ method, when voices are changed (or made available)
-function getVoices() {
+function retrieveVoices() {
   // retrieve the voices from the speechSynthesis object
-  const voices = speechSynthesis.getVoices();
+  const voices = synth.getVoices();
 
   // if available, store the first english-speaking voice you get in the option object
   if (voices) {
-    const englishVoices = voices.filter(voice => voice.lang === 'en-GB');
-    options.voices = englishVoices[0];
-
+    options.voice = voices.find(item => /en/gi.test(item.lang));
     // attach a listener for a click event on the button, which can now trigger the speechSynthesis object with the retrieved voice
     buttonSpeak.addEventListener('click', speak);
   }
 }
 
+retrieveVoices();
 // call the getVoices function when the onvoiceschanged method occurs
-speechSynthesis.onvoiceschanged = getVoices;
+speechSynthesis.onvoiceschanged = retrieveVoices;
