e69d7c5adca51c7f54ea8f788ff3afd0fbe1a45d
gabrielecorti <borntofrappe@gmail.com>
2019-01-15 00:24:11 +0100

    update the js code to work with the div of contentEditable

--- JS30 Speech Synthesis API/script.js
+++ JS30 Speech Synthesis API/script.js
@@ -3,10 +3,10 @@
 const synth = window.speechSynthesis;
 const utterance = new SpeechSynthesisUtterance();
 
-// store a reference to the button and input elements and the textarea
+// store a reference to the button and input elements and the contentEditable div
 const buttonSpeak = document.querySelector('button.button--speak');
 const inputRanges = document.querySelectorAll('input[type="range"]');
-const textarea = document.querySelector('textarea');
+const textInput = document.querySelector('div#features--text');
 // store a reference to the other two buttons
 const buttonToggle = document.querySelector('button.button--toggle');
 const buttonStop = document.querySelector('button.button--stop');
@@ -15,19 +15,36 @@ const buttonStop = document.querySelector('button.button--stop');
 // global variables:
 // - fallback, a sting passed to the synthesizer if the textarea element has no value
 // - isMouseInRange, to modify the options of the inputs of type range only when the mouse is down on them (and later moved)
-// - options, to alter the settings of the voice
 // - isPlaying, to toggle between pause and play (by default set to false, and toggle it to true whenever clicking the mic button)
-
-const fallback = 'Write something here, hit the mic and behold, the program talks back!';
+const fallback = 'Write something here, hit the mic and behold, the program talks back! Amazing, 🙌';
 let isMouseInRange = false;
 let isPlaying = false;
-// initialize  variable to keep track of the voice and other options
-const options = {
-  voice: '',
-  pitch: 1,
-  rate: 1
-};
 
+// initialize variable in which to store the voice and a variable in which to store the markup eventually injected in the contentEditable div
+let voice = '';
+let markup = '';
+
+// function called when retrieving the text input, to sanitize a few text elements requiring escaping
+// called as the second argument of the String.replace() function
+function handleBasicSanityCheck(match) {
+  // replace a matching value with the escaped counterpart
+  switch (match) {
+    case '&':
+      return '&amp;';
+    case '<':
+      return '&lt;';
+    case '>':
+      return '&gt;';
+    case '\"':
+      return '&quot;';
+    case '\'':
+      return '&#x27;';
+    case '/':
+      return '&#x2F;';
+    default:
+      return '';
+  }
+}
 
 // function called in response to the mousemove event on the input elements
 function handleMove(e) {
@@ -40,8 +57,8 @@ function handleMove(e) {
   const { target } = e;
   const { name, value } = target;
 
-  // change the option bearing the same name of the input element with the obtained value
-  options[name] = value;
+  // change the properties on the utterance instance to match with the new values
+  utterance[name] = value;
 }
 // toggle the boolean when the mouse is down/up on the input elements
 inputRanges.forEach(inputRange => inputRange.addEventListener('mousedown', () => isMouseInRange = true));
@@ -79,8 +96,8 @@ function handleStop() {
   buttonToggle.removeEventListener('click', handleToggle);
   buttonStop.removeEventListener('click', handleStop);
 
-  // empty the value of the textarea element
-  textarea.value = '';
+  // empty the value of the div container
+  textInput.textContent = '';
 
   // attach the event listener to the speak button, as to allow another instance of utterance
   // re-attacha class of active to show the hover state (mirror behavior with respect to the other two buttons)
@@ -88,6 +105,24 @@ function handleStop() {
   buttonSpeak.addEventListener('click', speak);
 }
 
+// function called in response to the onboundary event, to update the UI according to the spoken text
+function handleBoundary(e) {
+  // char index refers to the index at which the utterance hits a boundary
+  const { charIndex } = e;
+  // to avoid highlighting a word midway through, find the index of the nearest whitespace after this index
+  const { text } = e.currentTarget;
+  let lastIndex = text.indexOf(' ', charIndex);
+  // the index of the whitespace or the very last position in the string, to nicely wrap the entire text
+  if (lastIndex === -1) {
+    lastIndex = text.length;
+  }
+  // inject the text up to lastInde in a mark element, effectively highlighting it
+  markup = `
+      <mark>${text.substring(0, lastIndex)}</mark>
+      ${text.substring(lastIndex)}
+    `;
+  textInput.innerHTML = markup;
+}
 
 // function called in response to a click on the speak button
 // ! the event listener is attached on the button only when the voices from the speechSynthesis object are readily available
@@ -96,19 +131,25 @@ function speak() {
   isPlaying = true;
   buttonToggle.innerHTML = '<svg><use href="#voice-pause"</svg>';
 
-  // extract the text from the textarea element
-  const { value } = textarea;
+  // extract the text from the div container
+  const { textContent: value } = textInput;
 
-  // create an instance of SpeechSynthesisUtterance and use the .speak() method available on the synth object
-  // use the value of the textarea, or the fallback value
-  const speech = value || fallback;
+  // for the utterance, set the text to the value or a fallback if this returns false
+  let speech = fallback;
+  if (value) {
+    // sanitize the text covering a few special characters
+    speech = value.replace(/[&<>"'/]/gi, handleBasicSanityCheck);
+  } else {
+    // else set the text content to the fallback value, as to highlight this text as well
+    textInput.textContent = speech;
+  }
 
-  // text can be set from the textarea element to the text property of the utterance instance
+  // set the options specified by the speech and the (retrieved) voice
   utterance.text = speech;
-  // set the options specified in the options object
-  utterance.voice = options.voice;
-  utterance.rate = options.rate;
-  utterance.pitch = options.pitch;
+  utterance.voice = voice;
+
+  // when the utterance hits a boundary, call the handleBoundary function to update the UI
+  utterance.onboundary = handleBoundary;
 
   // when the utterance is over, call the handleStop function to terminate the current synth
   utterance.onend = handleStop;
@@ -134,7 +175,7 @@ function retrieveVoices() {
 
   // if available, store the first english-speaking voice you get in the option object
   if (voices) {
-    options.voice = voices.find(item => /en/gi.test(item.lang));
+    voice = voices.find(item => /en/gi.test(item.lang));
     // attach a listener for a click event on the button, which can now trigger the speechSynthesis object with the retrieved voice
     buttonSpeak.addEventListener('click', speak);
   }
